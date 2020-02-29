7b4dd58d0ada5e7f24e9a2b56216a40af3db0fb5
xiekun1992 <xkfront@outlook.com>
2020-03-01 00:18:31 +0800

    修改托盘图标

--- ui/tray.js
+++ ui/tray.js
@@ -1,4 +1,4 @@
-const {Tray, app, Menu} = require('electron')
+const {Tray, app, Menu, nativeImage} = require('electron')
 const path = require('path')
 const settingWindow = require('./setting_window')
 
@@ -7,17 +7,42 @@ function getInstance() {
     tray = new Tray(path.join(__dirname, 'images', 'tray.png'))
     const contextMenu = Menu.buildFromTemplate([
         {
-            label: 'Settings', type: 'normal', click(menuItem, browserWindow, event) {
+            icon: path.resolve(__dirname, './images/tray_menux16.png'), 
+            label: '设置', 
+            type: 'normal', 
+            click(menuItem, browserWindow, event) {
                 settingWindow.getInstance().show()
             }
         },
         {
-            label: 'Exit', type: 'normal', click(menuItem, browserWindow, event) {
+            icon: path.resolve(__dirname, './images/tray_menux16.png'),
+            label: '软件更新', 
+            type: 'normal', 
+            click(menuItem, browserWindow, event) {
+                
+            }
+        },
+        {
+            icon: path.resolve(__dirname, './images/tray_menux16.png'),
+            label: '用户反馈', 
+            type: 'normal', 
+            click(menuItem, browserWindow, event) {
+
+            }
+        },
+        {
+            icon: path.resolve(__dirname, './images/tray_menux16.png'),
+            label: '退出软件', 
+            type: 'normal', 
+            click(menuItem, browserWindow, event) {
                 app.quit()
             }
         }
     ])
     tray.setToolTip('lan_control')
+    tray.on('click', (event, bounds, position) => {
+        settingWindow.getInstance().show()
+    })
     tray.setContextMenu(contextMenu)
     return tray
 }
