6119d9b7dc4536c1252e13b9fcf1c3fb99add356
xiekun1992 <xkfront@outlook.com>
2020-03-11 01:47:37 +0800

    添加设备IP设置

--- signal/Device.js
+++ signal/Device.js
@@ -1,4 +1,5 @@
 const {screen} = require('electron')
+const os = require('os')
 const si = require('systeminformation')
 
 class Device {
@@ -24,13 +25,23 @@ class Device {
         const screenSize = screen.getPrimaryDisplay().size
         const osInfo = await si.osInfo()
         const system = await si.system()
+        const interfaces = os.networkInterfaces()
+        const addresses = {}
+        for (const name in interfaces) {
+            interfaces[name].forEach(item => {
+                if (item.family == 'IPv4' && item.address != '127.0.0.1') {
+                    // console.log(name, item.address)
+                    addresses[item.address] = item.mac
+                }
+            })
+        }
         return new Device({
             type: (await si.chassis()).type.toLowerCase(), 
             name: `${system.manufacturer} ${system.model} ${system.version}`.replace(/Not Applicable/gi, '').replace(/INVALID/gi, '').replace(/\-/gi, '').trim(),//'联想E49', 
             hostname: osInfo.hostname,//'联想E49', 
             os: `${osInfo.logofile} ${osInfo.release} ${osInfo.arch}`,// 'Linux Ubuntu x64', 
             resolution: `${screenSize.width}x${screenSize.height}`,//'1366x768', 
-            IP: ''// 接收方补充 '192.168.1.8'
+            IP: Object.keys(addresses).join(', ')// 接收方补充 '192.168.1.8'
         })
     }
 }
