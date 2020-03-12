a528ac042b4de42862c6531fc9980200ea8b1d11
xiekun1992 <xkfront@outlook.com>
2020-03-12 23:55:09 +0800

    travis

--- .travis-ci/add_release.sh
+++ .travis-ci/add_release.sh
@@ -2,7 +2,7 @@
 
 curl --location --request POST 'https://api.github.com/repos/xiekun1992/lan_control/releases' \
 --header 'Content-Type: application/json' \
---header 'Authorization: token 0d6b17a1b5782a7bbe8559a3e36e8dedddc924fd' \
+--header 'Authorization: token cc7077f99bcb4f12f69399d6e7abd83b70305012' \
 --data-raw '{
   "tag_name": "v1.0.0",
   "target_commitish": "master",
