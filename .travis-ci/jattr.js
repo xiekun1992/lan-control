d93f6680247af55695cf50a2f26ade51d7d4e94a
xiekun <xiekun@f3dt.com>
2020-03-15 02:43:47 +0800

    travis

--- .travis-ci/jattr.js
+++ .travis-ci/jattr.js
@@ -13,9 +13,10 @@
 //   process.stdout.write(target? JSON.stringify(target): '');
 // });
 
-console.log(process.argv)
+// console.log(process.argv)
 const fs = require('fs')
 const content = fs.readFileSync(process.argv[2]);
 const value = JSON.parse(content);
 const target = eval(`value.${process.argv[3]}`);
-process.stdout.write(target? JSON.stringify(target): '');
+fs.writeFileSync(process.argv[4], target? JSON.stringify(target): '');
+// process.stdout.write(target? JSON.stringify(target): '');
