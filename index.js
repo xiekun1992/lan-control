22512004ec0bdf7ada826e49dcca41e5f7bb27c5
xiekun1992 <xkfront@outlook.com>
2020-10-24 01:11:52 +0800

    keep single instance

--- index.js
+++ index.js
@@ -16,37 +16,45 @@ global.device = {
 }
 global.manualExit = false
 
-app.whenReady().then(async () => {
-  // auto startup
-  let autoLaunch = new AutoLaunch({
-    name: 'lan control',
-    path: app.getPath('exe')
-  })
-  autoLaunch.isEnabled().then((isEnabled) => {
-    if (!isEnabled) {
-      autoLaunch.enable()
-    }
-  })
+const singleInstanceLock = app.requestSingleInstanceLock()
 
-  tray.initTray()
-  await replay.start()
-  await discover.start()
-  clipboardNet.start()
-  clipboardNet.capture()
+if (!singleInstanceLock) {
+  global.manualExit = true
+  app.quit()
+} else {
+  app.whenReady().then(async () => {
+    // auto startup
+    let autoLaunch = new AutoLaunch({
+      name: 'lan control',
+      path: app.getPath('exe')
+    })
+    autoLaunch.isEnabled().then((isEnabled) => {
+      if (!isEnabled) {
+        autoLaunch.enable()
+      }
+    })
   
-  discover.event.on('discover', ({ devices, newDevice, thisDevice }) => {
-    global.device.remotes = devices
-    global.device.local = thisDevice
-    // console.log(devices, newDevice, thisDevice)
-    // tray.updateMenu(global.device.local)
-    // tray.updateMenu(global.device.remote)
+    tray.initTray()
+    await replay.start()
+    await discover.start()
+    clipboardNet.start()
+    clipboardNet.capture()
     
-    // if (!global.linux) {
-    //   capture.setConnectionPeer(devices[0].if)
-    //   capture.startCapture('right')
-    // }
+    discover.event.on('discover', ({ devices, newDevice, thisDevice }) => {
+      global.device.remotes = devices
+      global.device.local = thisDevice
+      // console.log(devices, newDevice, thisDevice)
+      // tray.updateMenu(global.device.local)
+      // tray.updateMenu(global.device.remote)
+      
+      // if (!global.linux) {
+      //   capture.setConnectionPeer(devices[0].if)
+      //   capture.startCapture('right')
+      // }
+    })
   })
-})
+}
+
 
 app.on('will-quit', (event) => {
   if (!global.manualExit) {
