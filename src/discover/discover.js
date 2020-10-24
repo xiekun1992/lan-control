65cc0a7e0b558b75876a2b6811b4f224292fa3b0
xiekun1992 <xkfront@outlook.com>
2020-10-24 21:21:31 +0800

    support network check

--- src/discover/discover.js
+++ src/discover/discover.js
@@ -1,4 +1,5 @@
 const Emitter = require('events')
+const { resolve } = require('path')
 const UDPPeer = require('./udp_peer')
 let hostInfo
 
@@ -6,7 +7,7 @@ const discoverEmitter = new Emitter()
 
 const port = 1234
 const multicastAddress = '224.0.0.114'
-const server = new UDPPeer(port).server
+let server = new UDPPeer(port).server
 
 let i = 0, timer, timeout = 1000
 const remoteDevices = []
@@ -17,13 +18,21 @@ function loop() {
     return
   }
   const item = hostInfo.nic[i]
-  server.setMulticastInterface(item.address)
-  const msg = Buffer.from(JSON.stringify(hostInfo))
-  server.send(msg, 0, msg.length, port, multicastAddress, (err, sentBytes) => {
-    // console.log(sentBytes)
-    i = (i + 1) % hostInfo.nic.length
-    sendAllInterfaces()
-  })
+  if (item) {
+    try {
+      server.setMulticastInterface(item.address) // manually close network may cause `Error: addMembership EINVAL`
+      const msg = Buffer.from(JSON.stringify(hostInfo))
+      server.send(msg, 0, msg.length, port, multicastAddress, (err, sentBytes) => {
+        // console.log(sentBytes)
+        i = (i + 1) % hostInfo.nic.length
+        sendAllInterfaces()
+      })
+    } catch (e) {
+      i = 0
+    }
+  } else {
+    i = 0
+  }
 }
 function sendAllInterfaces() {
   clearTimeout(timer)
@@ -61,14 +70,62 @@ module.exports = {
   event: discoverEmitter,
   async start() {
     hostInfo = await require('./utils').getHostInfo()
-    return new Promise((resolve) => {
-      server.bind(port, () => {
-        hostInfo.nic.forEach(item => {
+    server.bind(port, '0.0.0.0', () => {
+      hostInfo.nic.forEach(item => {
+        try {
           server.addMembership(multicastAddress, item.address)
-        })
-        sendAllInterfaces()
-        resolve()
+        } catch (e) {
+          console.error('addMembership fail: ', item.address, e.toString())
+        }
       })
+      sendAllInterfaces()
+    })
+
+    require('./utils').monitNetwork().on('update', (data) => {
+      hostInfo = data.hostInfo
+      const diffMinus = data.diffMinus
+      const diffAdd = data.diffAdd
+
+      // console.log(hostInfo.nic, diffMinus, diffAdd)
+      
+      // nodejs cannot listen network interface up and down, thus cannot properly dropmembership,
+      // restart server to solve this
+      if (server) {
+        server.close()
+      }
+      server = new UDPPeer(port).server
+      const delayTimer = setTimeout(() => { // wait for network card
+        clearTimeout(delayTimer)
+        server.bind(port, '0.0.0.0', () => {
+          hostInfo.nic.forEach(item => {
+            try {
+              server.addMembership(multicastAddress, item.address)
+            } catch (e) {
+              console.error('addMembership fail: ', item.address, e.toString())
+            }
+          })
+          sendAllInterfaces()
+        })
+      }, 3000)
+      // setTimeout(() => {
+      //   diffMinus.forEach(item => {
+      //     server.dropMembership(multicastAddress, item.address)
+      //   })
+      //   diffAdd.forEach(item => {
+      //     server.addMembership(multicastAddress, item.address)
+      //   })
+      //   // hostInfo.nic.forEach(item => {
+      //   //   try {
+      //   //     if (!membershipIPs[item.address]) {
+      //   //       server.addMembership(multicastAddress, item.address)
+      //   //       membershipIPs[item.address] = true
+      //   //     }
+      //   //   } catch (e) {
+      //   //     console.error('addMembership fail: ', item.address, e.toString())
+      //   //   }
+      //   // })
+      //   sendAllInterfaces()
+      // }, 3000);
     })
   },
   addToRemotesDevice(device) {
--- src/discover/utils.js
+++ src/discover/utils.js
@@ -1,71 +1,95 @@
 const si = require('systeminformation')
 const { screen } = require('electron')
 const os = require('os')
+const EventEmitter = require('events')
+
+async function getHostInfo() {
+  const screenSize = screen.getAllDisplays().map(item => `${item.size.width}x${item.size.height}`)//screen.getPrimaryDisplay().size
+  // const osInfo = await si.osInfo()
+  const system = await si.system()
+  const interfaces = await si.networkInterfaces()
+
+  let ifIpv4 = []
+  ifIpv4 = ifIpv4.concat(
+    interfaces.filter(
+      item => !item.internal && item.ip4
+    ).map((item) => {
+      const ipv4Decimals = item.ip4.split('.').map(item => +item)
+      const subnetDecimals = item.ip4subnet.split('.').map(item => +item)
+      const netIdDecimals = []
+      for (let i = 0; i < ipv4Decimals.length; i++) {
+        netIdDecimals[i] = ipv4Decimals[i] & subnetDecimals[i]
+      }
+      const netId = netIdDecimals.join('.')
+      return {address: item.ip4, mac: item.mac, mask: item.ip4subnet, netId }
+    })
+  )
+  return {
+    type: (await si.chassis()).type.toLowerCase(), 
+    name: `${system.manufacturer} ${system.model} ${system.version}`.replace(/Not Applicable/gi, '').replace(/INVALID/gi, '').replace(/\-/gi, '').trim(),//'联想E49', 
+    resolution: `${screenSize.join()}`,//'1366x768', 
+    cpuCores: os.cpus().length, // 8
+    cpuDesc: os.cpus()[0].model, // cpu specification
+    memory: Math.ceil(os.totalmem() / 1024 / 1024 / 1024) + 'GB',
+    nic: ifIpv4,
+    osInfo: `${os.type()} ${os.release()} ${os.arch()}`,
+    hostname: os.hostname(),
+    username: os.userInfo().username
+  }
+}
+
+class NetworkStatusEvent extends EventEmitter {}
+const netEvent = new NetworkStatusEvent()
+let nicNum = 0
+let prevNicInfo = []
+
+async function nicCheck() {
+  const ifs = os.networkInterfaces()
+  let currentNicNum = 0
+  for (const name in ifs) {
+    if (ifs[name].some(item => item.internal === false)) {
+      currentNicNum++
+    }
+  }
+  // check if new interface available or any interface is unavailable
+  if (nicNum !== currentNicNum) {
+    nicNum = currentNicNum
+    // update global device info
+    const hostInfo = await getHostInfo()
+    
+    let diffAdd = [], diffMinus = []
+    if (prevNicInfo.length === 0) {
+      diffAdd = hostInfo.nic
+    } else if (hostInfo.nic.length === 0) {
+      diffMinus = prevNicInfo // all interfaces down
+    } else {
+      for (const item of hostInfo.nic) { // part of interfaces up
+        if (!prevNicInfo.find(oldItem => oldItem.mac === item.mac)) {
+          diffAdd.push(item)
+        }
+      }
+      for (const oldItem of prevNicInfo) { // part of interfaces down
+        if (!hostInfo.nic.find(newItem => newItem.mac === oldItem.mac)) {
+          diffMinus.push(oldItem)
+        }
+      }
+    }
+    prevNicInfo = hostInfo.nic
+
+    global.device.local = hostInfo
+    netEvent.emit('update', {
+      hostInfo,
+      diffAdd, // tells wether interface up or down
+      diffMinus
+    })
+  }
+}
+
 
 module.exports = {
-  async getHostInfo() {
-    // return new Promise((resolve, reject) => {
-      const screenSize = screen.getAllDisplays().map(item => `${item.size.width}x${item.size.height}`)//screen.getPrimaryDisplay().size
-      // const osInfo = await si.osInfo()
-      const system = await si.system()
-      const interfaces = await si.networkInterfaces()
-
-      let ifIpv4 = []
-      ifIpv4 = ifIpv4.concat(
-        interfaces.filter(
-          item => !item.internal && item.ip4
-        ).map((item) => {
-          const ipv4Decimals = item.ip4.split('.').map(item => +item)
-          const subnetDecimals = item.ip4subnet.split('.').map(item => +item)
-          const netIdDecimals = []
-          for (let i = 0; i < ipv4Decimals.length; i++) {
-            netIdDecimals[i] = ipv4Decimals[i] & subnetDecimals[i]
-          }
-          const netId = netIdDecimals.join('.')
-          return {address: item.ip4, mac: item.mac, mask: item.ip4subnet, netId }
-        })
-      )
-      return {
-        type: (await si.chassis()).type.toLowerCase(), 
-        name: `${system.manufacturer} ${system.model} ${system.version}`.replace(/Not Applicable/gi, '').replace(/INVALID/gi, '').replace(/\-/gi, '').trim(),//'联想E49', 
-        resolution: `${screenSize.join()}`,//'1366x768', 
-        cpuCores: os.cpus().length, // 8
-        cpuDesc: os.cpus()[0].model, // cpu specification
-        memory: Math.ceil(os.totalmem() / 1024 / 1024 / 1024) + 'GB',
-        nic: ifIpv4,
-        osInfo: `${os.type()} ${os.release()} ${os.arch()}`,
-        hostname: os.hostname(),
-        username: os.userInfo().username
-      }
-
-      // si.networkInterfaces(function(interfaces) {
-
-      //   let ifIpv4 = []
-      //   ifIpv4 = ifIpv4.concat(
-      //     interfaces.filter(
-      //       item => !item.internal && item.ip4
-      //     ).map((item) => {
-      //       const ipv4Decimals = item.ip4.split('.').map(item => +item)
-      //       const subnetDecimals = item.ip4subnet.split('.').map(item => +item)
-      //       const netIdDecimals = []
-      //       for (let i = 0; i < ipv4Decimals.length; i++) {
-      //         netIdDecimals[i] = ipv4Decimals[i] & subnetDecimals[i]
-      //       }
-      //       const netId = netIdDecimals.join('.')
-      //       return {address: item.ip4, mac: item.mac, mask: item.ip4subnet, netId }
-      //     })
-      //   )
-      //   resolve({
-      //     cpuCores: os.cpus().length,
-      //     cpuDesc: os.cpus()[0].model,
-      //     memory: Math.ceil(os.totalmem() / 1024 / 1024 / 1024) + 'GB',
-      //     nic: ifIpv4,
-      //     osInfo: `${os.type()} ${os.release()} ${os.arch()}`,
-      //     hostname: os.hostname(),
-      //     username: os.userInfo().username
-      //   })
-      // })
-    // })
-  },
-
+  getHostInfo,
+  monitNetwork() {
+    setInterval(nicCheck, 1000)
+    return netEvent
+  }
 }
