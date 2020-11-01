c45aa1143dc0b473898f1b2fe6496b99c81ec920
xiekun1992 <xkfront@outlook.com>
2020-11-01 16:51:01 +0800

    fix #35

--- src/replay/replay.js
+++ src/replay/replay.js
@@ -11,39 +11,41 @@ let width, height
 server.on('message', (msg, rinfo) => {
   msg = JSON.parse(msg)
   // console.log(msg)
-  switch(msg.type) {
-    case 'mousemove': 
-      inputAuto.mousemove(
-        msg.x * width / global.device.remote.mapArea.width,
-        msg.y * height / global.device.remote.mapArea.height
-      )
-      break
-    case 'mousedown': 
-      inputAuto.mousedown(msg.button)
-      break
-    case 'mouseup': 
-      inputAuto.mouseup(msg.button)
-      break
-    case 'mousewheel': 
-      if (global.linux) {
-        inputAuto.mousewheel(msg.direction / -Math.abs(msg.direction))
-      } else {
-        inputAuto.mousewheel(msg.direction)
-      }
-      break
-    case 'mousewheel': 
-      if (global.linux) {
-        inputAuto.mousewheel(msg.direction / -Math.abs(msg.direction))
-      } else {
-        inputAuto.mousewheel(msg.direction)
-      }
-      break
-    case 'keydown': 
-      inputAuto.keydown(msg.char)
-      break
-    case 'keyup': 
-      inputAuto.keyup(msg.char)
-      break
+  if (global.device.remote) {
+    switch(msg.type) {
+      case 'mousemove': 
+        inputAuto.mousemove(
+          msg.x * width / global.device.remote.mapArea.width,
+          msg.y * height / global.device.remote.mapArea.height
+        )
+        break
+      case 'mousedown': 
+        inputAuto.mousedown(msg.button)
+        break
+      case 'mouseup': 
+        inputAuto.mouseup(msg.button)
+        break
+      case 'mousewheel': 
+        if (global.linux) {
+          inputAuto.mousewheel(msg.direction / -Math.abs(msg.direction))
+        } else {
+          inputAuto.mousewheel(msg.direction)
+        }
+        break
+      case 'mousewheel': 
+        if (global.linux) {
+          inputAuto.mousewheel(msg.direction / -Math.abs(msg.direction))
+        } else {
+          inputAuto.mousewheel(msg.direction)
+        }
+        break
+      case 'keydown': 
+        inputAuto.keydown(msg.char)
+        break
+      case 'keyup': 
+        inputAuto.keyup(msg.char)
+        break
+    }
   }
 })
 
