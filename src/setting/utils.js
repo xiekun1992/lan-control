4d6eba4ba7ada6e7d8f7eb37811e270d751d3444
xiekun1992 <xkfront@outlook.com>
2020-11-19 23:35:25 +0800

    refactor device connection

---
+++ src/setting/utils.js
@@ -0,0 +1,44 @@
+const http = require('http')
+
+function request(option) {
+  return new Promise((resolve, rejects) => {
+    const req = http.request(option, res => {
+      res.setEncoding('utf8')
+      let body = Buffer.alloc(0)
+      res.on('data', chunk => {
+        body = Buffer.concat([body, chunk])
+      })
+      res.on('end', () => {
+        resolve({
+          statusCode: res.statusCode,
+          headers: res.headers,
+          body
+        })
+      })
+    })
+    req.on('error', rejects)
+    req.end()
+  })
+}
+
+function connectDevice(ip, position, thisDevice) {
+  return request({
+    method: 'POST',
+    port: 2001,
+    hostname: ip,
+    path: `/connection?position=${position}&device=${JSON.stringify(thisDevice)}`
+  })
+}
+function disconnectDevice(ip, position, thisDevice) {
+  return request({
+    method: 'DELETE',
+    port: 2001,
+    hostname: ip,
+    path: `/connection?position=${position}&device=${JSON.stringify(thisDevice)}`
+  })
+}
+
+module.exports = {
+  connectDevice,
+  disconnectDevice
+}
