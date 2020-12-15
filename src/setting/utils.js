1363612841565093f6b8df35fee01b26f1b9101f
xiekun1992 <xkfront@outlook.com>
2020-12-16 00:16:29 +0800

    windows auto launch with admin privileges

--- src/setting/utils.js
+++ src/setting/utils.js
@@ -1,4 +1,6 @@
 const http = require('http')
+const AutoLaunch = require('auto-launch')
+const Registry = require('winreg')
 
 function request(option) {
   return Promise.race([
@@ -45,8 +47,61 @@ function disconnectDevice(ip, position, thisDevice) {
     path: `/connection?position=${encodeURIComponent(position)}&device=${encodeURIComponent(JSON.stringify(thisDevice))}`
   })
 }
+function enableAutoBoot() {
+  let autoLaunch = new AutoLaunch({
+    name: global.appName,
+    path: global.appPath
+  })
+  autoLaunch.enable()
+  // run as administrator can not auto launch, use schedule tasks instead
+  if (os.platform() === 'win32' && !process.argv.includes('--dev')) {
+  //  const { exec } = require('child_process')
+  //  const path = require('path')
+  //  // __dirname will be app.asar path after install
+  //  exec(`schtasks /create /f /tn "lan control auto start" /tr ${path.join(__dirname, '../../lan_control.exe')} /sc onlogon /rl highest`)
+    const regKey = new Registry({
+      hive: Registry.HKLM,
+      key:  '\\Software\\Microsoft\\Windows\\CurrentVersion\\Run'
+    })
+    regKey.set(global.appName, Winreg.REG_SZ, "\"" + global.appPath + "\"", function(err) {
+      if (err != null) {
+        return reject(err);
+      }
+      return resolve();
+    });
+  }
+}
+function disableAutoBoot() {
+  let autoLaunch = new AutoLaunch({
+    name: global.appName,
+    path: global.appPath
+  })
+  autoLaunch.disable()
+  // run as administrator can not auto launch, use schedule tasks instead
+  if (os.platform() === 'win32' && !process.argv.includes('--dev')) {
+  //  const { exec } = require('child_process')
+  //  const path = require('path')
+  //  // __dirname will be app.asar path after install
+  //  exec(`schtasks /create /f /tn "lan control auto start" /tr ${path.join(__dirname, '../../lan_control.exe')} /sc onlogon /rl highest`)
+    const regKey = new Registry({
+      hive: Registry.HKLM,
+      key:  '\\Software\\Microsoft\\Windows\\CurrentVersion\\Run'
+    })
+    regKey.remove(global.appName, function(err) {
+      if (err != null) {
+        if (err.message.indexOf('The system was unable to find the specified registry key or value') !== -1) {
+          return resolve(false);
+        }
+        return reject(err);
+      }
+      return resolve();
+    });
+  }
+}
 
 module.exports = {
   connectDevice,
-  disconnectDevice
+  disconnectDevice,
+  enableAutoBoot,
+  disableAutoBoot
 }
