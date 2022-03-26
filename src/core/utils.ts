af9b7986ab089b25096c96815673bc31c1a3a7fa
xiekun1992 <xkfront@outlook.com>
2022-03-26 16:00:27 +0800

    Fix: Fix linux auto launch

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -30,64 +30,56 @@ async function _nicCheck() {
   }
 }
 function enableAutoBoot() {
-  if (global.appState.platform.windows) {
-    // run as administrator can not auto launch
-    _winEnableAutoLaunch() 
-  } else {
-    let autoLaunch = new AutoLaunch({
-      name: global.appState.name,
-      path: global.appState.path
-    })
-    autoLaunch.enable()
+  if (!process.argv.includes('--dev')) {
+    if (global.appState.platform.windows) {
+      // run as administrator can not auto launch
+      _winEnableAutoLaunch() 
+    } else {
+      let autoLaunch = new AutoLaunch({
+        name: global.appState.name,
+        path: global.appState.exePath
+      })
+      autoLaunch.enable()
+    }
   }
 }
 function disableAutoBoot() {
-  if (global.appState.platform.windows) {
-    // run as administrator can not auto launchs
-    _winDisableAutoLaunch()
-  } else {
-    let autoLaunch = new AutoLaunch({
-      name: global.appState.name,
-      path: global.appState.path
-    })
-    autoLaunch.disable()  
+  if (!process.argv.includes('--dev')) {
+    if (global.appState.platform.windows) {
+      // run as administrator can not auto launchs
+      _winDisableAutoLaunch()
+    } else {
+      let autoLaunch = new AutoLaunch({
+        name: global.appState.name,
+        path: global.appState.exePath
+      })
+      autoLaunch.disable()  
+    }
   }
 }
 function _winEnableAutoLaunch() { // require administrator privilege
-  if (!process.argv.includes('--dev')) {
-    //  const { exec } = require('child_process')
-    //  const path = require('path')
-    //  // __dirname will be app.asar path after install
-    //  exec(`schtasks /create /f /tn "lan control auto start" /tr ${path.join(__dirname, '../../lan_control.exe')} /sc onlogon /rl highest`)
-    cp.exec(`reg query HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name}`, function(err, stdout, stderr) {
-      if (err) {
-        cp.exec(`reg add HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name} /t reg_sz /d ${global.appState.exePath} /f`)
-      }
-    })
-    cp.exec(`reg query HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name}`, function(err, stdout, stderr) {
-      if (err) {
-        cp.exec(`reg add HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name} /t reg_sz /d ${global.appState.exePath} /f`)
-      }
-    })
-  }
+  cp.exec(`reg query HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name}`, function(err, stdout, stderr) {
+    if (err) {
+      cp.exec(`reg add HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name} /t reg_sz /d ${global.appState.exePath} /f`)
+    }
+  })
+  cp.exec(`reg query HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name}`, function(err, stdout, stderr) {
+    if (err) {
+      cp.exec(`reg add HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name} /t reg_sz /d ${global.appState.exePath} /f`)
+    }
+  })
 }
 function _winDisableAutoLaunch() { // require administrator privilege
-  if (!process.argv.includes('--dev')) {
-    //  const { exec } = require('child_process')
-    //  const path = require('path')
-    //  // __dirname will be app.asar path after install
-    //  exec(`schtasks /create /f /tn "lan control auto start" /tr ${path.join(__dirname, '../../lan_control.exe')} /sc onlogon /rl highest`)
-    cp.exec(`reg query HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name}`, function(err, stdout, stderr) {
-      if (!err) {
-        cp.exec(`reg delete HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\ /v ${global.appState.name} /f`)
-      }
-    })
-    cp.exec(`reg query HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name}`, function(err, stdout, stderr) {
-      if (!err) {
-    cp.exec(`reg delete HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run\ /v ${global.appState.name} /f`)
-      }
-    })
-  }
+  cp.exec(`reg query HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name}`, function(err, stdout, stderr) {
+    if (!err) {
+      cp.exec(`reg delete HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\ /v ${global.appState.name} /f`)
+    }
+  })
+  cp.exec(`reg query HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run /v ${global.appState.name}`, function(err, stdout, stderr) {
+    if (!err) {
+  cp.exec(`reg delete HKLM\\Software\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Run\ /v ${global.appState.name} /f`)
+    }
+  })
 }
 
 export {
